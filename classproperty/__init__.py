1bfc8324b52a04ae592de297f9b4683fefd9490a
dpepper <pepper.daniel@gmail.com>
2018-02-07 12:43:36 -0800

    boom

---
+++ classproperty/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'dpepper'
+__version__ = '0.1.0'
+
+
+class classproperty(object):
+  def __init__(self, func):
+    self.func = func
+
+  def __get__(self, obj, owner):
+    args = []
+
+    if self.func.func_code.co_argcount > 0:
+      args.append(owner)
+
+    return self.func(*args)
---
+++ tests/Test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import os
+import sys
+import unittest
+
+sys.path = [ os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')) ] + sys.path
+from classproperty import classproperty
+
+
+class ArrayTest(unittest.TestCase):
+    def test_basic(self):
+        class Foo:
+            a = 'a'
+
+            @staticmethod
+            def b():
+                return 'b'
+
+            @classmethod
+            def c(cls):
+                return 'c'
+
+            def d(self):
+                return 'd'
+
+            @classproperty
+            def p(cls):
+                return 'p'
+
+
+        self.assertEquals(
+            Foo.a,
+            'a'
+        )
+
+        self.assertEquals(
+            Foo.b(),
+            'b'
+        )
+
+        self.assertEquals(
+            Foo.c(),
+            'c'
+        )
+
+        self.assertEquals(
+            Foo().d(),
+            'd'
+        )
+
+        self.assertEquals(
+            Foo.p,
+            'p'
+        )
+
+        self.assertEquals(
+            Foo().p,
+            'p'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
